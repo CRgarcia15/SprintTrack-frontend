ab07b276e8cfa3e31c8fe5e375ff6103e11e9d3a
CRgarcia15 <cg58708@gmial.com>
2022-11-23 05:10:49 -0500

    started working on the sing-up form

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div>
-        <h1>Welcome to SprintTrack</h1>
+        <h1 className='welcome-msg'>Welcome to SprintTrack</h1>
         <h3>Where you can keep track of any project you can come up with.</h3>
 
         <p>If you have an account with us please login. If you don't have an account with us, please make one to start tracking your projects.</p>
--- pages/signup.js
+++ pages/signup.js
@@ -1,33 +1,53 @@
-//make sure to add the inputs and labels correctly before adding the functionality
+import React, { useState } from "react"
+import { useNavigate } from 'react-router-dom'
 
-//This function takes care of signing up a user to use the app.
+//This function takes care of creating an user to use the app.
 function singup () {
+    const [ name, setName ] = useState('')
+    const [ username, setUsername ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const redirect = useNavigate
+
+    const handleSubmit = (e) => {
+        e.prenventDefault()
+        const user = { name, username, password}
+
+        fetch('http://localhost:7000/user', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user)
+        }).then(() => {
+            console.log('New user created')
+        })
+        redirect('/userHome')
+    }
+
     return (
         <div className="signup">
             
             <h1>Sign Up</h1>
 
-            <div className="form">
-                <label>Name</label>
+            <form className="create-user">
+                <label className="login-lable" htmlFor="name">Name</label>
                 <br/>
-                <input></input>
+                <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} required></input>
 
                 <br/>
 
-                <label>Username</label>
+                <label className="login-lable" htmlFor="username">Username</label>
                 <br/>
-                <input></input>
+                <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required></input>
 
                 <br/>
 
-                <label>Password</label>
+                <label className="login-lable" htmlFor="password">Password</label>
                 <br/>
-                <input></input>
+                <input type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
                 
                 <br />
 
-                <button className="submit-sign-up">Submit</button>
-            </div>
+                <button className="submit-sign-up" type="submit">Submit</button>
+            </form>
 
         </div>
     )
